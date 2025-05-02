89f3b81cfb09fda838e8490ce6fd57d29a03b9e6
AragornDev <andres.dellaporta.dev@gmail.com>
2025-05-02 13:16:53 -0300

    Adding simple test

--- src/test/java/com/example/restservice/greeting/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/greeting/GreetingControllerTests.java
@@ -111,40 +111,17 @@
 //  * See the License for the specific language governing permissions and
 //  * limitations under the License.
 //  */
-// package com.example.restservice.greeting;
+package com.example.restservice.greeting;
 
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-// import org.junit.jupiter.api.Test;
+public class GreetingControllerTests {
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.web.servlet.MockMvc;
+	@Test
+	public void simpleTest() {
+		String name = "andres";
+		Assertions.assertNotNull(name, "Awesome! name is not null");
+	}
 
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// public class GreetingControllerTests {
-
-// 	@Autowired
-// 	private MockMvc mockMvc;
-
-// 	@Test
-// 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-// 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-// 				.andExpect(jsonPath("$.content").value("Hello, World!"));
-// 	}
-
-// 	@Test
-// 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-// 		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-// 				.andDo(print()).andExpect(status().isOk())
-// 				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-// 	}
-
-// }
+}
